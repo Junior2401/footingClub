24d7b370fc36ad5d9a4d25d507ee57d11a367aea
Superadmin <Superadmin@L-PNR22-2021.SUPERMARKET.local>
2024-01-08 17:23:38 +0100

    Visualisation de la liste des licenciés par catégorie

---
+++ src/Controller/LicencieController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\HttpFoundation\Request;
+
+class LicencieController extends AbstractController
+{
+    #[Route('/licencies/{id?}', name: 'licencies')]
+    public function index(Connection $connection, Request $request): Response
+    {
+
+        $id = $request->query->get('id');
+
+        $categories = $connection->fetchAllAssociative('SELECT * FROM categories');
+
+        $sql = '
+            SELECT * FROM licencies
+            WHERE categorie_id = :id
+            ORDER BY id DESC
+        ';
+
+
+        $resultSet = $connection->executeQuery($sql, ['id' => $id]);
+
+        $licencies = $resultSet->fetchAllAssociative();
+
+        $req = $id;       
+
+        if($id == null){
+            $sql = '
+                SELECT * FROM licencies ORDER BY id DESC
+            ';
+            $licencies = $connection->fetchAllAssociative($sql);
+
+            $req = 0;
+        }
+        
+
+
+        return $this->render('licencie/index.html.twig', [
+            'licencies' => $licencies,
+            'categories' => $categories,
+            'req' => $req,
+        ]);
+    }
+}
