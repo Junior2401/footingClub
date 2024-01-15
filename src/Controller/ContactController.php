729a4a04bec74c47ebeb5e3255a30466189855d7
Superadmin <Superadmin@L-PNR22-2021.SUPERMARKET.local>
2024-01-15 17:01:42 +0100

    Gestion d'authentification

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -68,20 +68,118 @@ class ContactController extends AbstractController
         ]);
     }
     
-    
+
+
     #[Route('/contacts/email', name: 'email_contacts')]
     public function email(): Response
     {
-        $sql = "SELECT mail.*, contacts.* FROM mail_contacts mail JOIN mail_contact_contacts mee ON mail.id = mee.mail_contact_id JOIN contacts ON mee.mail_contact_id = contacts.id";
+        $sql = "SELECT mail_contact_contacts.id AS id, contacts.nom AS nom, contacts.prenom AS prenom, mail_contacts.objet AS objet, mail_contacts.message AS message, mail_contacts.dateEnvoi AS dateEnvoi, contacts.email AS email
+                FROM mail_contact_contacts
+                JOIN mail_contacts ON mail_contact_contacts.mail_contact_id = mail_contacts.id
+                JOIN contacts ON mail_contact_contacts.contact_id = contacts.id
+                ORDER BY mail_contacts.dateEnvoi DESC";
 
-        $resultSet = $this->connection->executeQuery($sql);
-
-        $results = $resultSet->fetchAllAssociative();
-
+        $results = $this->connection->executeQuery($sql)->fetchAllAssociative();
     
         return $this->render('contact/email.html.twig', [
             'results' => $results,
         ]);
     }
 
+
+    #[Route('/contacts/create', name: 'manage_email_contact', methods: ['GET','POST'])]
+    public function create(Request $request): Response
+    {
+
+        if ($request->isMethod('POST')) {
+            // Récupérer les données du formulaire
+            $objet = $request->request->get('objet');
+            $message = $request->request->get('message');
+            $contactIds = $request->get('contactIds');
+
+            // Insérer le mailEdu dans la table mail_contact
+            $sql = 'INSERT INTO mail_contacts (objet, message) VALUES (?, ?)';
+            $this->connection->executeUpdate($sql, [$objet, $message]);
+
+            // Récupérer l'ID du dernier enregistrement inséré
+            $sql = 'SELECT * FROM mail_contacts ORDER BY id DESC LIMIT 1';
+            $mailContactId = $this->connection->executeQuery($sql)->fetchAssociative();
+
+            // Insérer les relations dans la table mail_contact_contact
+            foreach ($contactIds as $contact) {
+                $sql = 'INSERT INTO mail_contact_contacts (mail_contact_id, contact_id) VALUES (?, ?)';
+                $contactr = $this->connection->executeUpdate($sql, [$mailContactId['id'], $contact]);
+            }
+
+            $this->addFlash('message', 'Votre message a été envoyé au contacts avec succès.');
+            // Redirection ou autre logique après l'insertion
+            return $this->redirectToRoute('email_contacts');
+
+        }        
+        $sql = "SELECT * 
+                FROM contacts";
+
+        $results = $this->connection->executeQuery($sql)->fetchAllAssociative();
+        
+        return $this->render('contact/create.html.twig', [
+            'results' => $results,
+        ]);
+    }
+
+
+    #[Route('/contacts/email/show/{id}', name: 'manage_email_show', methods: ['GET'])]
+    public function show($id): Response
+    {
+
+        $sql = "SELECT mail_contact_contacts.id AS id, contacts.nom AS nom, contacts.prenom AS prenom, mail_contacts.objet AS objet, mail_contacts.message AS message, mail_contacts.dateEnvoi AS dateEnvoi, contacts.email AS email
+                FROM mail_contact_contacts
+                JOIN mail_contacts ON mail_contact_contacts.mail_contact_id = mail_contacts.id
+                JOIN contacts ON mail_contact_contacts.contact_id = contacts.id
+                WHERE mail_contact_contacts.id = :id
+                ORDER BY mail_contacts.dateEnvoi DESC";
+
+        $params = ['id' => $id];
+
+        $result = $this->connection->executeQuery($sql, $params)->fetchAssociative();
+        
+        return $this->render('contact/show.html.twig', [
+            'result' => $result,
+        ]);
+    }
+
+
+    #[Route('/contacts/email/delete/{id}', name: 'manage_email_delete', methods: ['GET', 'POST'])]
+    public function delete(Request $request, $id): Response
+    {
+        if ($request->isMethod('POST')) {
+            // Récupérer les données du formulaire
+
+            $sql = 'DELETE FROM mail_contact_contacts WHERE id = :id';
+            
+            $params = ['id'=>$id];
+        
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute($params);
+
+            $this->addFlash('message', 'Votre message a été supprimé au contact avec succès.');
+            // Redirection ou autre logique après l'insertion
+            return $this->redirectToRoute('email_contacts');
+
+        } 
+
+        $sql = "SELECT mail_contact_contacts.id AS id, contacts.nom AS nom, contacts.prenom AS prenom, mail_contacts.objet AS objet, mail_contacts.message AS message, mail_contacts.dateEnvoi AS dateEnvoi, contacts.email AS email
+                FROM mail_contact_contacts
+                JOIN mail_contacts ON mail_contact_contacts.mail_contact_id = mail_contacts.id
+                JOIN contacts ON mail_contact_contacts.contact_id = contacts.id
+                WHERE mail_contact_contacts.id = :id
+                ORDER BY mail_contacts.dateEnvoi DESC";
+
+        $params = ['id' => $id];
+
+        $result = $this->connection->executeQuery($sql, $params)->fetchAssociative();
+        
+        return $this->render('contact/delete.html.twig', [
+            'result' => $result,
+        ]);
+    } 
 }
--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,14 +5,55 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\DBAL\Connection;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard/{id?}', name: 'dashboard')]
-    public function index(): Response
+    public function index(Connection $connection): Response
     {
+
+        $sql1 = '
+            SELECT COUNT(*) AS licence_count FROM licencies ORDER BY id DESC
+        ';
+        $licencies = $connection->fetchAssociative($sql1);
+        //$licencies = $licencies['count'];
+
+        $sql2 = "SELECT COUNT(mail_edu_educateurs.id) AS mail_educateur_count
+                    FROM mail_edu_educateurs
+                    JOIN mail_edu ON mail_edu_educateurs.mail_edu_id = mail_edu.id
+                    JOIN educateurs ON mail_edu_educateurs.educateur_id = educateurs.id
+                    JOIN licencies ON educateurs.licencie_id = licencies.id
+                    ORDER BY mail_edu.dateEnvoi DESC";
+
+        $emaileducateurs = $connection->fetchAssociative($sql2);
+
+
+        $sql3 = '
+            SELECT COUNT(*)  AS contact_count FROM contacts
+            WHERE EXISTS (
+                SELECT * 
+                FROM licencies 
+                WHERE contacts.id = licencies.contact_id
+            )
+            ORDER BY contacts.id DESC
+        ';
+        $contacts = $connection->fetchAssociative($sql3);
+
+
+        $sql4 = "SELECT COUNT(mail_contact_contacts.id) AS mail_contact_count
+                FROM mail_contact_contacts
+                JOIN mail_contacts ON mail_contact_contacts.mail_contact_id = mail_contacts.id
+                JOIN contacts ON mail_contact_contacts.contact_id = contacts.id
+                ORDER BY mail_contacts.dateEnvoi DESC";
+
+        $emailcontacts = $connection->fetchAssociative($sql4);        
+                
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'licencies' => $licencies,
+            'emaileducateurs' => $emaileducateurs,
+            'contacts' => $contacts,
+            'emailcontacts' => $emailcontacts,
         ]);
     }
 }
